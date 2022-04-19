63f90b0bfc7a0c8108ed7cd8b39aad0d366ec254
perfectogo <jasurbek.go.dev@gmail.com>
2022-04-19 23:23:33 +0500

    hello, world

---
+++ 1_helloWorld/README.md
@@ -0,0 +1,7 @@
+# RabbitMQ
+
+* RabbitMQ bu message brokerdir, U messaglarni qabul qiladi va uzatadi
+* RabbitMQ va pochta bo'limi o'rtasidagi asosiy farqi shundaki, u qog'oz bilan ishlamaydi, buning o'rniga u ma'lumotlarning byte bloklarini qabul qiladi, saqlaydi va yo'naltiradi.
+* Queue - RabbitMQ ichida joylashgan pochta qutisining nomi.
+
+### using the Go RabbitMQ client
---
+++ 1_helloWorld/receive/receive.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"log"
+
+	"github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+func main() {
+	// we open a connection and a channel, and declare the queue from which we're going to consume. Note this matches up with the queue that send publishes to.
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+	//Note that we declare the queue here, as well. Because we might start the consumer before the publisher, we want to make sure the queue exists before we try to consume messages from it.
+	//We're about to tell the server to deliver us the messages from the queue. Since it will push us messages asynchronously, we will read the messages from a channel (returned by amqp::Consume) in a goroutine.
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
+}
+
+//The consumer will print the message it gets from the publisher via RabbitMQ. The consumer will keep running, waiting for messages (Use Ctrl-C to stop it), so try running the publisher from another terminal.
